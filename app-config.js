cb0e7878b433b618d17c21bac9133b5a855d0824
yybcg7ygvm-tech <yybcg7ygvm@privaterelay.appleid.com>
2026-08-23 17:06:28 +0300

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
 window.NITEK_CONFIG = {
   SUPABASE_URL: "https://kwiqjwojrowwooukmjih.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_..."
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_di373iSfOTaPUlSfcasZbg_RHvQJDle"
 };
