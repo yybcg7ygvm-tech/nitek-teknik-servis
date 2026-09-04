1b4968034daf893f5f300a2a414182cceb5dab9f
yybcg7ygvm-tech <yybcg7ygvm@privaterelay.appleid.com>
2026-09-04 19:30:55 +0300

    Add files via upload

--- faults-loader.js
+++ faults-loader.js
@@ -1,1 +1,13 @@
-window.NITEK_FAULT_GUIDE={version:"brand-files-test-1.0",description:"Demirdöküm ve Bosch ayrı dosyalardan yüklenir.",models:Object.assign({},(window.NITEK_FAULTS_DEMIRDOKUM&&window.NITEK_FAULTS_DEMIRDOKUM.models)||{},(window.NITEK_FAULTS_BOSCH&&window.NITEK_FAULTS_BOSCH.models)||{})};
+// NİTEK marka dosyalarını doğru marka > model yapısında birleştirir.
+(function(){
+  const d=window.NITEK_FAULTS_DEMIRDOKUM;
+  const b=window.NITEK_FAULTS_BOSCH;
+  window.NITEK_FAULT_GUIDE={
+    version:"brand-files-test-2.0",
+    description:"Marka dosyaları ayrı yüklenir.",
+    models:{
+      ...(d?.models ? {"Demirdöküm":d.models} : {}),
+      ...(b?.models ? {"Bosch":b.models} : {})
+    }
+  };
+})();
